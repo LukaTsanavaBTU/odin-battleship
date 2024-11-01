67c9517450d3ffbdfb3aafe676f6d88825df0241
LukaTsanavaBTU <luka.tsanava.1@btu.edu.ge>
2024-11-01 14:44:56 +0400

    Make more progress on computer attack logic

--- src/scripts/ComputerPlayer.js
+++ src/scripts/ComputerPlayer.js
@@ -3,12 +3,6 @@ import { getRandomInt, randomIndex } from "./helpers";
 
 function ComputerPlayer() {
     const board = Gameboard();
-    const memory = {
-        attacking: false,
-        lastHit: [],
-        axis: null,
-        queue: []
-    };
 
     function getBoard() {
         return board;
@@ -32,14 +26,29 @@ function ComputerPlayer() {
         placeRandomShip(2);
     }
 
-    // Tired, do all of this later
+    // Attack Logic
+    const memory = {
+        axis: null,
+        queue: [],
+    };
+
+    function clearMemory() {
+        memory = {
+            axis: null,
+            queue: [],
+        };
+    }
 
     function attack(target) {
         if (memory["attacking"]) {
             if (memory["axis"] !== null) {
-
+                const queue = (memory["axis"] === 0) ? memory["queueX"] : memory["queueY"];
+                const index = randomIndex(queue);
+                const element = queue[index];
+                queue.splice(index, 1)
+                attackAt(target, element);
             } else {
-                const index = randomIndex(memory["queue"]);
+                const index = randomIndex([...memory["queueX"], ...memory["queueY"]]);
                 const element = memory["queue"][index];
                 memory["queue"].splice(index, 1);
                 attackAt(target, element);
@@ -60,22 +69,44 @@ function ComputerPlayer() {
 
     }
 
-    function attackAt(target, [x, y]) {
+    function attackAt(target, {x, y, axis}) {
         const targetBoard = target.getBoard();
-        targetBoard.receiveAttack([randX, randY]);
-        const hitCell = targetBoard.getCoordinates([randX, randY]);
+        targetBoard.receiveAttack([x, y]);
+        const hitCell = targetBoard.getCoordinates([x, y]);
         if (hitCell["ship"] && !hitCell["ship"].isSunk()) {
-            memory["attacking"] = true;
-            memory["lastHit"] = [randX, randY];
-            [
-                [randX + 1, randY], [randX - 1, randY], 
-                [randX, randY + 1], [randX, randY - 1]
-            ].forEach((coordinate) => {
-                if (targetBoard.checkCoordinateValidity(coordinate) &&
-                    !targetBoard.getCoordinates(coordinate)["isHit"]) {
-                        memory["queue"].push(coordinate);
-                }
+            const neighbors = [
+                {coordinate: [x + 1, y], axis: 0}, 
+                {coordinate: [x - 1, y], axis: 0}, 
+                {coordinate: [x, y + 1], axis: 1}, 
+                {coordinate: [x, y - 1], axis: 1}
+            ].filter((cell) => {
+                return targetBoard.checkCoordinateValidity(cell["coordinate"])
+                    && !targetBoard.getCoordinates(cell["coordinate"])["isHit"];
             });
+            if (!memory["axis"] && axis) {
+                // If the axis is unknown and it isn't our first hit, find out the axis
+                memory["axis"] = axis;
+                memory["queue"] = memory["queue"].filter((cell) => cell["axis"] === axis);
+                
+            }
+            if (!memory["axis"]) {
+                // If axis is unknown and it is our first hit, add all valid neighboring cells to queue  
+                neighbors.forEach((cell) => {
+                    memory["queue"].push(cell);
+                });
+            } else {
+                // If axis is known, add valid neighboring cells only on that axis 
+                neighbors.forEach((cell) => {
+                    if (cell["axis"] === memory["axis"]) {
+                        memory["queue"].push(cell);
+                    }
+                });
+            }
+        } else if ((hitCell["ship"] && hitCell["ship"].isSunk()) || (memory["queue"].length === 0)) {
+            // There will be a bug here if there are several ships right next to each other, fix later
+            // Keep track of hit ship cells
+            // If queue is empty and ship has not been sunk, enqueue all hit ship cells and start attacking on opposite axis 
+            clearMemory();
         }
     }
 
