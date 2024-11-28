d3b2af746a6c38246a76ca543acee8cdfce92f60
LukaTsanavaBTU <luka.tsanava.1@btu.edu.ge>
2024-11-28 21:26:08 +0400

    Add functions and styles to dynamically draw gameboard

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
-const grids = document.querySelectorAll(".grid");
+import {drawGridEnemy, drawGridPlayer} from "./scripts/domFunctions.js";
+import HumanPlayer from "./scripts/HumanPlayer.js";
+import ComputerPlayer from "./scripts/ComputerPlayer.js";
 
-grids.forEach((grid) => {
-    for (let i = 0; i < 100; i++) {
-        const gridCell = document.createElement("div");
-        gridCell.classList.add("grid-cell");
-        grid.appendChild(gridCell);
-    }
-});
+const player = HumanPlayer();
+const playerBoard = player.getBoard();
+playerBoard.placeShip([2, 2], 0, 3);
+playerBoard.placeShip([7, 4], 1, 4);
+playerBoard.placeShip([1, 6], 0, 2);
+playerBoard.getCoordinates([1, 6])["isHit"] = true;
+playerBoard.getCoordinates([3, 8])["isHit"] = true;
+
+const enemy = ComputerPlayer();
+const enemyBoard = enemy.getBoard();
+enemyBoard.placeShip([5, 3], 1, 3);
+enemyBoard.placeShip([2, 4], 1, 4);
+enemyBoard.placeShip([7, 2], 0, 2);
+
+drawGridEnemy(enemy);
+drawGridPlayer(player);
---
+++ src/scripts/domFunctions.js
@@ -0,0 +1,60 @@
+function drawGridPlayer(player) {
+    const grid = document.querySelector(".grid.player");
+    const board = player.getBoard();
+    for (let y = 0; y < 10; y++) {
+        for (let x = 0; x < 10; x++) {
+            const cell =  board.getCoordinates([x, y]);
+            const gridCell = document.createElement("div");
+            gridCell.classList.add("grid-cell");
+            gridCell.classList.add(cell["direction"]);
+            if (cell["ship"]) {
+                gridCell.classList.add("ship")
+            }
+            if (cell["isHit"] && cell["ship"]) {
+                gridCell.classList.add("hit-strike");
+                const strikeImg = document.createElement("img");
+                strikeImg.setAttribute("src", "./media/hit-strike.png");
+                const hitMarker = document.createElement("div");
+                hitMarker.classList.add("strike-marker");
+                hitMarker.appendChild(strikeImg);
+                gridCell.appendChild(hitMarker);
+            } else if (cell["isHit"] && !cell["ship"]) {
+                gridCell.classList.add("hit-miss");
+                const hitMarker = document.createElement("div");
+                hitMarker.classList.add("miss-marker");                
+                gridCell.appendChild(hitMarker);
+            }
+            gridCell.dataset.x = x;
+            gridCell.dataset.y = y;
+            grid.appendChild(gridCell);
+        }
+    }   
+    
+}
+
+function drawGridEnemy(enemy) {
+    const grid = document.querySelector(".grid.enemy");
+    const board = enemy.getBoard();
+    for (let y = 0; y < 10; y++) {
+        for (let x = 0; x < 10; x++) {
+            const cell =  board.getCoordinates([x, y]);
+            const gridCell = document.createElement("div");
+            gridCell.classList.add("grid-cell");
+            if (cell["ship"]) {
+                gridCell.classList.add("ship")
+            }
+            if (cell["isHit"]) {
+                gridCell.classList.add("is-hit");
+                const hitMarker = document.createElement("div");
+                hitMarker.classList.add("hit-marker");
+                gridCell.appendChild(hitMarker);
+            }
+            gridCell.dataset.x = x;
+            gridCell.dataset.y = y;
+            grid.appendChild(gridCell);
+        }
+    }   
+    
+}
+
+export {drawGridPlayer, drawGridEnemy};
